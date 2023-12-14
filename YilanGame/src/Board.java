6a079cc06db840775db144721d4a57feff35070b
Taha <tahadursunxx@gmail.com>
2023-12-14 22:45:02 +0300

    Game Over "Oyun Bitti" olarak değiştirildi.

--- YilanGame/src/Board.java
+++ YilanGame/src/Board.java
@@ -15,12 +15,12 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int B_WIDTH = 300;
-    private final int B_HEIGHT = 300;
+    private final int B_WIDTH = 800;
+    private final int B_HEIGHT = 800;
     private final int DOT_SIZE = 10;
     private final int ALL_DOTS = 900;
-    private final int RAND_POS = 29;
-    private final int DELAY = 140;
+    private final int RAND_POS = 79;
+    private final int DELAY = 100;
 
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
@@ -114,7 +114,7 @@ public class Board extends JPanel implements ActionListener {
 
     private void gameOver(Graphics g) {
 
-        String msg = "Oyun Bittii";
+        String msg = "Oyun Bitti";
         Font small = new Font("Helvetica", Font.BOLD, 14);
         FontMetrics metr = getFontMetrics(small);
 
