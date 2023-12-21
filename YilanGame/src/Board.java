01895389af0433639ff6b09dfc9344011b1eb138
DAVUTCAN <davutcan5858600@gmail.com>
2023-12-22 00:14:46 +0300

    Oyunun arka planı yeşillikle değiştirildi, oyun skoru sağ üst köşeye eklendi, oyun bitince ortaya skor puanıda yazdırıldı.

--- YilanGame/src/Board.java
+++ YilanGame/src/Board.java
@@ -13,231 +13,254 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+// Board sınıfı, JPanel sınıfından türetiliyor ve ActionListener arayüzünü uyguluyor.
+// Bu, Board'un bir grafik bileşen olduğu ve zamanlayıcıdan gelen olaylara yanıt verebileceği anlamına gelir.
 public class Board extends JPanel implements ActionListener {
 
-    private final int B_WIDTH = 300;
-    private final int B_HEIGHT = 300;
-    private final int DOT_SIZE = 10;
-    private final int ALL_DOTS = 900;
-    private final int RAND_POS = 29;
-    private final int DELAY = 100;
+    private final int B_WIDTH = 400; // Oyun alanının genişliği (piksel cinsinden)
+    private final int B_HEIGHT = 400; // Oyun alanının yüksekliği (piksel cinsinden)
+    private final int DOT_SIZE = 10; // Yılanın her bir parçasının boyutu (piksel cinsinden)
+    private final int ALL_DOTS = 900; // Yılanın maksimum uzunluğu (parça cinsinden)
+    private final int RAND_POS = 29; // Yemin rastgele konumlandırılması için kullanılan sabit
+    private final int DELAY = 150; // Zamanlayıcının gecikme süresi (milisaniye cinsinden)
+    private int score;//BU SATIRI DAVUT EKLEDİ.
 
-    private final int x[] = new int[ALL_DOTS];
-    private final int y[] = new int[ALL_DOTS];
+    private final int x[] = new int[ALL_DOTS]; // Yılanın x koordinatlarını tutan dizi
+    private final int y[] = new int[ALL_DOTS]; // Yılanın y koordinatlarını tutan dizi
 
-    private int dots;
-    private int apple_x;
-    private int apple_y;
+    private int dots; // Yılanın mevcut uzunluğu (parça cinsinden)
+    private int apple_x; // Yemin x koordinatı
+    private int apple_y; // Yemin y koordinatı
 
-    private boolean leftDirection = false;
-    private boolean rightDirection = true;
-    private boolean upDirection = false;
-    private boolean downDirection = false;
-    private boolean inGame = true;
+    private boolean leftDirection = false; // Yılanın sola dönüş durumu
+    private boolean rightDirection = true; // Yılanın sağa dönüş durumu
+    private boolean upDirection = false; // Yılanın yukarı dönüş durumu
+    private boolean downDirection = false; // Yılanın aşağı dönüş durumu
+    private boolean inGame = true; // Oyunun devam etme durumu
 
-    private Timer timer;
-    private Image ball;
-    private Image apple;
-    private Image head;
+    private Timer timer; // Zamanlayıcı nesnesi
+    private Image ball; // Yılanın her bir parçasının resmi
+    private Image apple; // Yemin resmi
+    private Image head; // Yılanın başının resmi
+    private Image bgImage;//BU SATIRI DAVUT EKLEDİ.
 
-    public Board() {
+    public Board() { // Board sınıfının kurucu metodu
 
-        initBoard();
+        initBoard(); // Oyun alanını başlatan metodu çağırır
     }
 
-    private void initBoard() {
+    private void initBoard() { // Oyun alanını başlatan metod
 
-        addKeyListener(new TAdapter());
-        setBackground(Color.black);
-        setFocusable(true);
+        addKeyListener(new TAdapter()); // Klavye kontrolleri için bir dinleyici ekler
+        setFocusable(true); // Klavye kontrolleri için odaklanabilirliği açar
 
-        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-        loadImages();
-        initGame();
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT)); // Oyun alanının boyutunu ayarlar
+        loadImages(); // Resimleri yükleyen metodu çağırır
+        initGame(); // Oyunu başlatan metodu çağırır
     }
 
-    private void loadImages() {
+    private void loadImages() { // Resimleri yükleyen metod
 
-        ImageIcon iid = new ImageIcon("C:\\Users\\Ahmet\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources/dot.png");
+        //BU SATIRI DAVUT EKLEDİ
+        ImageIcon iib= new ImageIcon("C:\\Users\\DAVUTCAN\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources\\background.jpg");
+        bgImage= iib.getImage();
+
+
+        ImageIcon iid = new ImageIcon("C:\\Users\\DAVUTCAN\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources/dot.png"); // Yılanın her bir parçasının resmini yükler
         ball = iid.getImage();
 
-        ImageIcon iia = new ImageIcon("C:\\Users\\Ahmet\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources/apple.png");
+        ImageIcon iia = new ImageIcon("C:\\Users\\DAVUTCAN\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources/apple.png"); // Yemin resmini yükler
         apple = iia.getImage();
 
-        ImageIcon iih = new ImageIcon("C:\\Users\\Ahmet\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources/head.png");
+        ImageIcon iih = new ImageIcon("C:\\Users\\DAVUTCAN\\Desktop\\2023GrafikYilan\\YilanGame\\src\\resources/head.png"); // Yılanın başının resmini yükler
         head = iih.getImage();
+
+
     }
 
-    private void initGame() {
+    private void initGame() { // Oyunu başlatan metot
+        score = 0; //BU SATIRI DAVUT EKLEDİ
+        dots = 4; // Yılanın başlangıç uzunluğunu ayarlar
 
-        dots = 3;
-
-        for (int z = 0; z < dots; z++) {
-            x[z] = 50 - z * 10;
-            y[z] = 50;
+        for (int z = 0; z < dots; z++) { // Yılanın başlangıç konumunu belirler
+            x[z] = 50 - z * 10; // Yılanın x koordinatlarını sırayla azaltır
+            y[z] = 50; // Yılanın y koordinatlarını sabit tutar
         }
 
-        locateApple();
+        locateApple(); // Yemin konumunu belirleyen metodu çağırır
 
-        timer = new Timer(DELAY, this);
-        timer.start();
+        timer = new Timer(DELAY, this); // Zamanlayıcı nesnesini oluşturur ve gecikme süresi ve dinleyici olarak Board sınıfını verir
+        timer.start(); // Zamanlayıcıyı başlatır
     }
 
     @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        doDrawing(g);
+    public void paintComponent(Graphics g) { // Oyun alanını çizen metot
+        super.paintComponent(g); // Üst sınıfın metodunu çağırır
+        g.drawImage(bgImage, 0, 0,getWidth(),getHeight(),this);     //BU SATIRI DAVUT EKLEDİ
+        doDrawing(g); // Oyun alanını çizen yardımcı metodu çağırır
     }
 
-    private void doDrawing(Graphics g) {
+    private void doDrawing(Graphics g) { // Oyun alanını çizen yardımcı metot
 
-        if (inGame) {
+        if (inGame) { // Eğer oyun devam ediyorsa
 
-            g.drawImage(apple, apple_x, apple_y, this);
+            g.drawImage(apple, apple_x, apple_y, this); // Yemin resmini yeminin konumunda çizer
 
-            for (int z = 0; z < dots; z++) {
-                if (z == 0) {
-                    g.drawImage(head, x[z], y[z], this);
-                } else {
-                    g.drawImage(ball, x[z], y[z], this);
+            String msg = "Skor: " + score; // Skoru gösteren mesaj
+            Font small = new Font("Helvetica", Font.BOLD, 14); // Mesajın fontu
+            FontMetrics metr = getFontMetrics(small); // Mesajın metrikleri
+            g.setColor(Color.white); // Mesajın rengi
+            g.setFont(small); // Mesajın fontu
+            g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) - 5, 15); // Mesajı ekrana yazar
+
+            for (int z = 0; z < dots; z++) { // Yılanın her bir parçası için
+                if (z == 0) { // Eğer ilk parça ise
+                    g.drawImage(head, x[z], y[z], this); // Yılanın başının resmini çizer
+                } else { // Eğer ilk parça değilse
+                    g.drawImage(ball, x[z], y[z], this); // Yılanın diğer parçalarının resmini çizer
                 }
             }
 
-            Toolkit.getDefaultToolkit().sync();
+            Toolkit.getDefaultToolkit().sync(); // Grafik sistemi ile senkronize eder
 
-        } else {
+        } else { // Eğer oyun bitmişse
 
-            gameOver(g);
+            gameOver(g); // Oyun bittiğini gösteren metodu çağırır
         }
     }
 
-    private void gameOver(Graphics g) {
+    private void gameOver(Graphics g) { // Oyun bittiğini gösteren metot
 
-        String msg = "Oyun Bitmiştir";
-        Font small = new Font("Helvetica", Font.BOLD, 14);
-        FontMetrics metr = getFontMetrics(small);
+        String msg = "KAYBETTİNİZ :("   ; // Oyun bittiğini belirten mesaj
+        String m_score = "PUANINIZ: "+ score;
+        Font small = new Font("İtalic", Font.BOLD, 20); // Mesajın fontu
+        FontMetrics metr = getFontMetrics(small); // Mesajın metrikleri
 
-        g.setColor(Color.white);
-        g.setFont(small);
-        g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
+        g.setColor(Color.red); // Mesajın rengi
+        g.setFont(small); // Mesajın fontu
+        g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2); // Mesajı ekrana yazar
+        g.drawString(m_score, (B_WIDTH - metr.stringWidth(m_score)) / 2, (B_HEIGHT / 2)+20); // Mesajı ekrana yazar
+
     }
 
-    private void checkApple() {
+    private void checkApple() { // Yılanın yemi yiyip yemediğini kontrol eden metot
 
-        if ((x[0] == apple_x) && (y[0] == apple_y)) {
+        if ((x[0] == apple_x) && (y[0] == apple_y)) { // Eğer yemi yemişse
 
-            dots++;
-            locateApple();
-        }
-    }
-
-    private void move() {
-
-        for (int z = dots; z > 0; z--) {
-            x[z] = x[(z - 1)];
-            y[z] = y[(z - 1)];
-        }
-
-        if (leftDirection) {
-            x[0] -= DOT_SIZE;
-        }
-
-        if (rightDirection) {
-            x[0] += DOT_SIZE;
-        }
-
-        if (upDirection) {
-            y[0] -= DOT_SIZE;
-        }
-
-        if (downDirection) {
-            y[0] += DOT_SIZE;
+            dots++; // Yılanın uzunluğunu arttırır
+            score += 10;    //BU SATIRI DAVUT EKLEDİ
+            locateApple(); // Yeni bir yem konumu belirler
         }
     }
 
-    private void checkCollision() {
+    private void move() { // Yılanın hareket etmesini sağlayan metot
 
-        for (int z = dots; z > 0; z--) {
+        for (int z = dots; z > 0; z--) { // Yılanın parçalarının koordinatlarını günceller
+            x[z] = x[(z - 1)]; // Bir sonraki parçanın x koordinatını alır
+            y[z] = y[(z - 1)]; // Bir sonraki parçanın y koordinatını alır
+        }
 
-            if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
-                inGame = false;
+        if (leftDirection) { // Eğer yılan sola dönüyorsa
+            x[0] -= DOT_SIZE; // Başının x koordinatını azaltır
+        }
+
+        if (rightDirection) { // Eğer yılan sağa dönüyorsa
+            x[0] += DOT_SIZE; // Başının x koordinatını arttırır
+        }
+
+        if (upDirection) { // Eğer yılan yukarı dönüyorsa
+            y[0] -= DOT_SIZE; // Başının y koordinatını azaltır
+        }
+
+        if (downDirection) { // Eğer yılan aşağı dönüyorsa
+            y[0] += DOT_SIZE; // Başının y koordinatını arttırır
+        }
+    }
+
+    private void checkCollision() { // Yılanın kendisine veya duvarlara çarpmasını kontrol eden metot
+
+        for (int z = dots; z > 0; z--) { // Yılanın her bir parçası için
+
+            if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) { // Eğer baş, vücudun bir parçasına çarpmışsa
+                inGame = false; // Oyunu bitirir
             }
         }
 
-        if (y[0] >= B_HEIGHT) {
-            y[0] = 0;
+        if (y[0] >= B_HEIGHT) { // Eğer baş, alt duvara çarpmışsa
+            y[0] = 0; // Başın y koordinatını sıfırlar
         }
 
-        if (y[0] < 0) {
-            y[0] = B_HEIGHT;
+        if (y[0] < 0) { // Eğer baş, üst duvara çarpmışsa
+            y[0] = B_HEIGHT; // Başın y koordinatını oyun alanının yüksekliği yapar
         }
 
-        if (x[0] >= B_WIDTH) {
-            x[0] = 0;
+        if (x[0] >= B_WIDTH) { // Eğer baş, sağ duvara çarpmışsa
+            x[0] = 0; // Başın x koordinatını sıfırlar
         }
 
-        if (x[0] < 0) {
-            x[0] = B_WIDTH;
+        if (x[0] < 0) { // Eğer baş, sol duvara çarpmışsa
+            x[0] = B_WIDTH; // Başın x koordinatını oyun alanının genişliği yapar
         }
 
-        if (!inGame) {
-            timer.stop();
+        if (!inGame) { // Eğer oyun bitmişse
+            timer.stop(); // Zamanlayıcıyı durdurur
         }
     }
 
-    private void locateApple() {
 
-        int r = (int) (Math.random() * RAND_POS);
-        apple_x = ((r * DOT_SIZE));
+    private void locateApple() { // Yemin konumunu rastgele belirleyen metot
 
-        r = (int) (Math.random() * RAND_POS);
-        apple_y = ((r * DOT_SIZE));
+        int r = (int) (Math.random() * RAND_POS); // 0 ile RAND_POS arasında rastgele bir sayı üretir
+        apple_x = ((r * DOT_SIZE)); // Yemin x koordinatını, yılanın boyutuna göre ayarlar
+
+        r = (int) (Math.random() * RAND_POS); // 0 ile RAND_POS arasında rastgele bir sayı üretir
+        apple_y = ((r * DOT_SIZE)); // Yemin y koordinatını, yılanın boyutuna göre ayarlar
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) { // Zamanlayıcıdan gelen olaylara yanıt veren metot
 
-        if (inGame) {
+        if (inGame) { // Eğer oyun devam ediyorsa
 
-            checkApple();
-            checkCollision();
-            move();
+            checkApple(); // Yılanın yemi yiyip yemediğini kontrol eden metodu çağırır
+            checkCollision(); // Yılanın kendisine veya duvarlara çarpmasını kontrol eden metodu çağırır
+            move(); // Yılanın hareket etmesini sağlayan metodu çağırır
         }
 
-        repaint();
+        repaint(); // Oyun alanını yeniden çizen metodu çağırır
     }
 
-    private class TAdapter extends KeyAdapter {
+    private class TAdapter extends KeyAdapter { // Klavye kontrollerini sağlayan iç sınıf
 
         @Override
-        public void keyPressed(KeyEvent e) {
+        public void keyPressed(KeyEvent e) { // Klavyeden basılan tuşlara göre yılanın yönünü değiştiren metot
 
-            int key = e.getKeyCode();
+            int key = e.getKeyCode(); // Basılan tuşun kodunu alır
 
-            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
-                leftDirection = true;
-                upDirection = false;
-                downDirection = false;
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) { // Eğer sol ok tuşu basılmışsa ve yılan sağa gitmiyorsa
+                leftDirection = true; // Yılanın sola dönüş durumunu açar
+                upDirection = false; // Yılanın yukarı dönüş durumunu kapatır
+                downDirection = false; // Yılanın aşağı dönüş durumunu kapatır
             }
 
-            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
-                rightDirection = true;
-                upDirection = false;
-                downDirection = false;
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) { // Eğer sağ ok tuşu basılmışsa ve yılan sola gitmiyorsa
+                rightDirection = true; // Yılanın sağa dönüş durumunu açar
+                upDirection = false; // Yılanın yukarı dönüş durumunu kapatır
+                downDirection = false; // Yılanın aşağı dönüş durumunu kapatır
             }
 
-            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
-                upDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+            if ((key == KeyEvent.VK_UP) && (!downDirection)) { // Eğer yukarı ok tuşu basılmışsa ve yılan aşağı gitmiyorsa
+                upDirection = true; // Yılanın yukarı dönüş durumunu açar
+                rightDirection = false; // Yılanın sağa dönüş durumunu kapatır
+                leftDirection = false; // Yılanın sola dönüş durumunu kapatır
             }
 
-            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
-                downDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) { // Eğer aşağı ok tuşu basılmışsa ve yılan yukarı gitmiyorsa
+                downDirection = true; // Yılanın aşağı dönüş durumunu açar
+                rightDirection = false; // Yılanın sağa dönüş durumunu kapatır
+                leftDirection = false; // Yılanın sola dönüş durumunu kapatır
             }
         }
     }
 }
+
--- YilanGame/src/Snake.java
+++ YilanGame/src/Snake.java
@@ -2,31 +2,31 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-public class Snake extends JFrame {
+public class Snake extends JFrame { // Snake sınıfı, JFrame sınıfından türetiliyor
 
-    public Snake() {
+    public Snake() { // Snake sınıfının kurucu metodu
 
-        initUI();
+        initUI(); // Pencereyi başlatan metodu çağırır
     }
 
-    private void initUI() {
+    private void initUI() { // Pencereyi başlatan metod
 
-        add(new Board());
+        add(new Board()); // Oyun alanını temsil eden Board nesnesini pencereye ekler
 
-        setResizable(false);
-        pack();
+        setResizable(false); // Pencerenin boyutunun değiştirilememesini sağlar
+        pack(); // Pencerenin boyutunu içerdiği bileşenlere göre ayarlar
 
-        setTitle("Yılan Oyunu");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Yılan Oyunu"); // Pencerenin başlığını belirler
+        setLocationRelativeTo(null); // Pencerenin ekranın ortasında konumlanmasını sağlar
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Pencere kapatıldığında programın sonlanmasını sağlar
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Programın başlangıç noktası
 
-        EventQueue.invokeLater(() -> {
-            JFrame ex = new Snake();
-            ex.setVisible(true);
+        EventQueue.invokeLater(() -> { // Olay kuyruğunda bir görev oluşturur
+            JFrame ex = new Snake(); // Snake nesnesini oluşturur
+            ex.setVisible(true); // Pencereyi görünür yapar
         });
     }
 }
